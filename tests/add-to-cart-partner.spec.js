ca9b6944b7f3cb4c6f8a0806f5e0d3d7ca45878c
kaekulsiri <kae.kulsiri@q-chang.com>
2024-12-24 15:01:41 +0700

    Add and Modify keywords

--- tests/add-to-cart-partner.spec.js
+++ tests/add-to-cart-partner.spec.js
@@ -5,16 +5,18 @@ test('Successful add to cart - Marketplace', async ({ page }) => {
     await keywords.OpenPartnerPage(page)
     await keywords.PartnerSelectProduct(page)
     await keywords.PartnerCustAddress(page,'สีบัวทอง', '11', 'Landmark')
-    await keywords.PartnerCustDetail(page, 'AutomatedTest', 'QATest', '0634844124', 'kaefortest@gmail.com')
+    await keywords.PartnerCustDetail(page, 'MarketplaceAutomated', 'SurveyTest', '0634844124', 'kaefortest@gmail.com')
     await keywords.ConfirmBasedOnConditions(page, 'survey', 'marketplace')
     await keywords.PaidByCreditCard(page, '4111-1111-1111-1111', '12/24', 'AutomatedTest', '123', '123456')
+    await keywords.DisplaySuccessPage(page)
 });
 
 test('Successful add to cart - Retail', async ({ page }) => {
     await keywords.OpenPartnerPage(page)
     await keywords.PartnerSelectProduct(page)
     await keywords.PartnerCustAddress(page,'สีบัวทอง', '11', 'Landmark')
-    await keywords.PartnerCustDetail(page, 'AutomatedTest', 'QATest', '0634844124', 'kaefortest@gmail.com')
+    await keywords.PartnerCustDetail(page, 'RetailAutomated', 'SurveyTest', '0634844124', 'kaefortest@gmail.com')
     await keywords.PartnerApplyCoupon(page,'retailautomate')
     await keywords.ConfirmBasedOnConditions(page, 'survey', 'retail')
+    await keywords.DisplaySuccessPage(page)
 });
